292c3b687a8f80dddc0d23007e895b7abcb9a529
Veritas-Lux <gaotd2008@gmail.com>
2025-09-01 15:47:07 -0500

    Created test set and data vis

--- src/Chapter 2 full project.py
+++ src/Chapter 2 full project.py
@@ -2,6 +2,10 @@ import pandas as pd
 import numpy as np
 import sklearn
 
+#Housing prices in California
+
+############################BASIC DATA#####################################
+
 #Download the data
 import os
 import tarfile
@@ -27,5 +31,56 @@ def load_housing_data(housing_path=HOUSING_PATH):
     return pd.read_csv(csv_path)
 
 #Look at the data
-housing = load_housing_data()
-housing.head() #First five rows of dataset
+housing = load_housing_data() #the main dataset
+print(housing.head()) #First five rows of dataset
+print(housing.info()) #Displays data types and information like missing values, total values, etc
+print(housing["ocean_proximity"].value_counts()) # for categorical data
+print(housing.describe()) #general data like quartiles, max, min, mean, std
+
+#histiograms to visualize data spread
+import matplotlib.pyplot as plt
+housing.hist(bins=50, figsize=(20,15))
+plt.savefig("Housing_attribute_histogram_plots.png")
+#plt.show()
+
+#################################TEST SET#################################################
+from sklearn.model_selection import train_test_split
+
+#Default test set
+train_set, test_set = train_test_split(housing, test_size=0.2, random_state=42)
+print(test_set.head())
+
+#Test set representative of the median income distribution
+
+#seperate median income into categories
+housing["median_income"].hist()
+housing["income_cat"] = pd.cut(housing["median_income"],
+                               bins=[0., 1.5, 3.0, 4.5, 6., np.inf],
+                               labels=[1, 2, 3, 4, 5])
+print(housing["income_cat"].value_counts())
+housing["income_cat"].hist()
+
+from sklearn.model_selection import StratifiedShuffleSplit
+
+split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+for train_index, test_index in split.split(housing, housing["income_cat"]): #picks test data proportional to median income distribution
+    strat_train_set = housing.loc[train_index] #training set
+    strat_test_set = housing.loc[test_index] #testing set
+
+#see how close the proportions are
+print(strat_test_set["income_cat"].value_counts() / len(strat_test_set))
+print(housing["income_cat"].value_counts() / len(housing))
+
+for set_ in (strat_train_set, strat_test_set): #we don't need the income categories anymore
+    set_.drop("income_cat", axis=1, inplace=True)
+
+#################################DATA VIS AND CLEAN#################################################
+housing = strat_test_set.copy() #now work with a copy of the training data
+
+#basic scatterplot that sort of looks like California
+housing.plot(kind="scatter", x="longitude", y="latitude", alpha=0.4,
+             s=housing["population"]/100, label="population", figsize=(10,7),
+             c="median_house_value", cmap=plt.get_cmap("jet"), colorbar=True,
+             sharex=False)
+plt.legend()
+save_fig("housing_prices_scatterplot")
